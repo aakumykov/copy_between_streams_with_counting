74473e1fbced2d1c1e0195fd3e1b87dcff910997
Andrey Kumykov <aakumykov@yandex.ru>
2026-05-02 10:30:01 +0300

    Удалил из метода необязательные аргументы. Всё равно ошибка.

--- app/src/main/java/com/github/aakumykov/copy_between_streams_with_counting_demo/MainActivity.kt
+++ app/src/main/java/com/github/aakumykov/copy_between_streams_with_counting_demo/MainActivity.kt
@@ -58,13 +58,6 @@ class MainActivity : AppCompatActivity(), FileSelector.Callbacks {
                         copyBetweenStreamsWithCounting(
                             inputStream = inputStream,
                             outputStream = fileOutputStream,
-                            requiredSpeedBytesPerSecond = { requiredSpeedBytePerSecond },
-                            speedCallback = {
-                                Log.d(TAG, "скорость: ${humanReadableByteCount(it.roundToLong())}")
-                            },
-                            afterWriteCallback = {
-                                Thread.sleep(500L)
-                            }
                         )
                     }
                 }
