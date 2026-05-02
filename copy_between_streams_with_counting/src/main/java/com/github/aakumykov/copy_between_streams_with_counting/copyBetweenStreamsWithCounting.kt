ac4fb1f672a9d9bb72ec1ac218250306d1650e24
Andrey Kumykov <aakumykov@yandex.ru>
2026-05-02 11:34:42 +0300

    fix: имя функции; v0.0.13-alpha

--- copy_between_streams_with_counting/src/main/java/com/github/aakumykov/copy_between_streams_with_counting/copyBetweenStreamsWithCounting.kt
+++ copy_between_streams_with_counting/src/main/java/com/github/aakumykov/copy_between_streams_with_counting/copyBetweenStreamsWithCounting.kt
@@ -24,7 +24,7 @@ import java.io.OutputStream
 v0.0.11-alpha
  */
 @Throws(IOException::class)
-fun copyBetweenStreamsWithCounting2(
+fun copyBetweenStreamsWithCounting(
     inputStream: InputStream,
     outputStream: OutputStream,
     requiredSpeedBytesPerSecond: Supplier<Long> = Supplier { -1 },
